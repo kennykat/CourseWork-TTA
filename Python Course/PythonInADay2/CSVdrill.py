ac578c617118dc892c4552807b648fda0c99f0a5
Kendra Iraheta <irahetakendra@gmail.com>
2016-04-05 17:47:50 -0700

    added webscraper file

--- Python Course/PythonInADay2/CSVdrill.py
+++ Python Course/PythonInADay2/CSVdrill.py
@@ -19,3 +19,15 @@ csvFile = open(outputCsv, "wb")
 # create writer object
 writer = csv.writer(csvFile, delimiter=',')
 # remember to use module csv
+
+# data to go in csv
+row_1 = [1, "Row 1", 123]
+row_2 = [2, "Row 2", 456]
+row_3 = [3, "Row 3", 789]
+
+# .writerow()
+# write rows to csv
+
+writer.writerow(row_1)
+writer.writerow(row_2)
+writer.writerow(row_3)
