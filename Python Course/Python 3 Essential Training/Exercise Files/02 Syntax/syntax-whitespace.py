e29f85fc2088594342d88feba37f3bb624e509c9
Kendra Iraheta <irahetakendra@gmail.com>
2016-04-19 11:19:36 -0700

    camel-cased files names

--- Python Course/Python 3 Essential Training/Exercise Files/02 Syntax/syntax-whitespace.py
+++
@@ -1,7 +0,0 @@
-#!/usr/local/bin/python3
-
-def main():
-    print("This is the syntax.py file.")
-    # print("this is another line")
-
-if __name__ == "__main__": main()
--- Python Course/Python 3 Essential Training/Exercise Files/03 Variables/variables.py
+++
@@ -1,6 +0,0 @@
-#!/usr/local/bin/python3
-
-def main():
-    print("This is the variables.py file.")
-
-if __name__ == "__main__": main()
