25360b0dabb201d3475180111c1ffe3c9c579327
Kendra Iraheta <irahetakendra@gmail.com>
2017-02-06 13:54:30 -0800

    added more code

--- PythonAgain/basicPython.py
+++ PythonAgain/basicPython.py
@@ -1,12 +1,33 @@
 ''' -- Item 36 of 68 -- '''
 
-# assigning int to variable
-variable = 10
+# 1. assigning int to variable
+shoeSize = 10
 
-# assigning string to variable
-variable = "string"
+# 2. assigning string to variable
+shoeBrand = "Adidas"
 
-# assigning float to variable
-variable = 3.0
+# 3. assigning float to variable
+shoePrice = 59.99
 
-# print function and .format() notation to print assigned variable
+# 4. print function and .format() notation to print assigned variable
+print shoeBrand.upper()
+
+# 5. operators
+x = 10
+y = 5
+
+print x + y
+print x - y
+print x * y
+print x / y
+
+x += 2
+print x
+
+print x % y
+
+# 6. logical operators
+
+
+
+# 7. conditional statements
