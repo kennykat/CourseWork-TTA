1fed25de1680c90207a3c1331377e173e88e124d
Kendra Iraheta <irahetakendra@gmail.com>
2017-02-05 16:18:48 -0800

    deleted pythontest.py, adding item 36: basicPython.py

---
+++ PythonAgain/basicPython.py
@@ -0,0 +1,12 @@
+''' -- Item 36 of 68 -- '''
+
+# assigning int to variable
+variable = 10
+
+# assigning string to variable
+variable = "string"
+
+# assigning float to variable
+variable = 3.0
+
+# print function and .format() notation to print assigned variable
