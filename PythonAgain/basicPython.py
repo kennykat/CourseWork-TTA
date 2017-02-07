60f8377e13099947d1558066896e537394aba5ac
Kendra Iraheta <irahetakendra@gmail.com>
2017-02-06 19:55:32 -0800

    Added more code

--- PythonAgain/basicPython.py
+++ PythonAgain/basicPython.py
@@ -1,18 +1,22 @@
 ''' -- Item 36 of 68 -- '''
 
-# 1. assigning int to variable
+print "\n1. assigning int to variable"
 shoeSize = 10
+print shoeSize
 
-# 2. assigning string to variable
+print "\n2. assigning string to variable"
 shoeBrand = "Adidas"
+print shoeBrand
 
-# 3. assigning float to variable
+print "\n3. assigning float to variable"
 shoePrice = 59.99
+print shoePrice
 
-# 4. print function and .format() notation to print assigned variable
-print shoeBrand.upper()
+print "\n4. print function and .format() notation to print assigned variable"
 
-# 5. operators
+print "{} is the price for the size {} {} shoes.".format(shoePrice,shoeSize,shoeBrand)
+
+print "\n5. operators"
 x = 10
 y = 5
 
@@ -26,8 +30,25 @@ print x
 
 print x % y
 
-# 6. logical operators
+print "\n6 & 7. logical operators & conditional statements"
 
+if x == 300:
+    print "x is not equal to 300"
 
+elif x and y == 10:
+    print "x and y are equal to 10"
 
-# 7. conditional statements
+else:
+    print "else statement is working"
+
+if x or y == 6:
+    print "either x or y are equal to 5"
+
+if not x == 10:
+    print "x does not equal 10"
+
+print "\n8 & 9. while and for loops"
+
+while (x < 4):
+    x = x + 1
+    print x
