31511ff9a06d96d81e897736b673fece4c9013e9
Kendra Iraheta <irahetakendra@gmail.com>
2017-02-25 15:51:34 -0800

    range() function drill

---
+++ PythonAgain/rangeFunction.py
@@ -0,0 +1,14 @@
+# using range() function in a for loop
+
+for i in range(4):
+    print(i)
+
+print('\n')
+
+for i in range(3,0,-1):
+    print(i)
+
+print('\n')
+
+for i in range(8,0,-2):
+    print(i)
