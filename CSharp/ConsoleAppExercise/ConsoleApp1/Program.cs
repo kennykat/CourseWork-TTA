18159f5edc33344f9cf5c9487ef55f7c7472de98
Kendra Iraheta <irahetakendra@gmail.com>
2018-05-26 13:03:39 -0700

    new and modified files

---
+++ CSharp/ConsoleAppExercise/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("What is your favorite number?");
+            string favoriteNumber = Console.ReadLine();
+            int favNum = Convert.ToInt32(favoriteNumber);
+            int total = favNum * 50;
+            Console.WriteLine(favNum + " multiplied by 50 is " + total + ".");
+            Console.WriteLine();
+            //Console.ReadLine();
+
+            Console.WriteLine("How old are you?");
+            string ageInput = Console.ReadLine();
+            int age = Convert.ToInt32(ageInput);
+            int totalAge = age + 25;
+            int youngAge = age - 12.5;
+            Console.WriteLine("In 25 years you will be " + totalAge + " years old.");
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
